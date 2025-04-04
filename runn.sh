2ee220702d7b774d101d5e3e0da5d4938a9e4e6a
ggskunk <137412331+ggskunk@users.noreply.github.com>
2025-04-04 23:59:03 +0300

    Update runn.sh

--- runn.sh
+++ runn.sh
@@ -8,7 +8,7 @@ while true; do
     # Python ile -k değerini oluştur
     K_VALUE=$(python3 -c "import random; print(random.randint($START, $END))")
 
-    for F in {1..7}; do
+    for F in {1..5}; do
         echo "Running: ./mutagen -p 68 -k $K_VALUE -f $F"
         ./mutagen -p 68 -k $K_VALUE -f $F
     done
