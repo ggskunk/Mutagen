b11d90d49e12b59095b52e7e13fae18d51efb0fe
ggskunk <137412331+ggskunk@users.noreply.github.com>
2025-04-04 22:55:01 +0300

    Add files via upload

---
+++ runn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start ve End aralığı
+START=147573952589676412928
+END=295147905179352825855
+
+while true; do
+    # Python ile -k değerini oluştur
+    K_VALUE=$(python3 -c "import random; print(random.randint($START, $END))")
+
+    for F in {1..5}; do
+        echo "Running: ./mutagen -p 68 -k $K_VALUE -f $F"
+        ./mutagen -p 68 -k $K_VALUE -f $F
+    done
+done
